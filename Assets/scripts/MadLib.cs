70d03511605c8faf053de12877c4c1110d9feac4
Zuoming Shi <zuomingshi@gmail.com>
2016-12-03 12:32:59 -0800

    MadList class structures

---
+++ Assets/scripts/MadLib.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MadLib : MonoBehaviour {
+	
+	public string[] story;
+	public MadLibCandidates[] candidates;
+
+	public void StartSelection() {
+
+	}
+
+	public void StartSentences() {
+
+	}
+
+}
