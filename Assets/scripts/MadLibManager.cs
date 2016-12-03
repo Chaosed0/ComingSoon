9291a0c1d95469e0246abf2c7550b10c92e75c91
chaosed0 <chaosed0@gmail.com>
2016-12-03 15:18:17 -0800

    Start the game after some time

--- Assets/scripts/MadLibManager.cs
+++ Assets/scripts/MadLibManager.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class MadLibManager : MonoBehaviour {
 
 	public MadLib[] madlibs;
 	private int currentLibIndex = 0;
 
+    public float delayBetweenMadlibs = 1.0f;
+    public _Mono leftCurtain;
+    public _Mono rightCurtain;
+
+    public float curtainTarget = 20.0f;
+    public float curtainTime = 2.0f;
+
 	// Use this for initialization
 	void Start () {
-		//StartNextMadlib();
         ClearCandidateChoices();
+
+        Sequence seq = DOTween.Sequence();
+        seq.AppendInterval(1.0f);
+        seq.Insert(1.0f, DOTween.To(()=> leftCurtain.x, x => leftCurtain.x = x, -curtainTarget, curtainTime));
+        seq.Insert(1.0f, DOTween.To(()=> rightCurtain.x, x => rightCurtain.x = x, curtainTarget, curtainTime));
+        seq.AppendInterval(2.0f);
+        seq.AppendCallback(StartNextMadlib);
 	}
 
 	void StartNextMadlib() {
