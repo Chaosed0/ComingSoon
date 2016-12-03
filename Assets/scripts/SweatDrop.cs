5a1d67b6d37708b29bb99726468cd9a29b8626e1
chaosed0 <chaosed0@gmail.com>
2016-12-03 10:41:30 -0800

    Add sweating dude

---
+++ Assets/scripts/SweatDrop.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweatDrop : _Mono {
+    public float dropFormationTime = 1.0f;
+    public float dropAccelerationTime = 2.0f;
+    public float finalDropSpeed = 3.0f;
+
+    public float initialScale = 0.1f;
+    public float finalScale = 1.0f;
+
+    enum DropState {
+        Forming,
+        Accelerating,
+        Falling
+    }
+
+    DropState state = DropState.Forming;
+    float timer = 0.0f;
+    float moveSpeed = 0.0f;
+
+	void Start () {
+        transform.localScale = new Vector2(initialScale, initialScale);
+	}
+
+	void Update () {
+        timer += Time.deltaTime;
+        if (state == DropState.Forming) {
+            float interp = Util.easeInQuad(timer / dropFormationTime);
+            float scale = initialScale + interp * (finalScale - initialScale);
+            transform.localScale = new Vector2(scale, scale);
+
+            if (timer >= dropFormationTime) {
+                transform.localScale = new Vector2(finalScale, finalScale);
+                state = DropState.Accelerating;
+                timer = 0.0f;
+            }
+        } else if (state == DropState.Accelerating) {
+            float interp = Util.easeInQuad(timer / dropAccelerationTime);
+            float speed = interp * finalDropSpeed;
+            moveSpeed = speed;
+
+            if (timer >= dropAccelerationTime) {
+                moveSpeed = finalDropSpeed;
+                state = DropState.Falling;
+                timer = 0.0f;
+            }
+        }
+
+        this.y -= moveSpeed * Time.deltaTime;
+	}
+}
---
+++ Assets/scripts/SweatSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweatSpawner : MonoBehaviour {
+    public float sweatSpawnTime = 1.0f;
+    public SweatDrop sweatPrefab;
+
+    BoxCollider2D sweatSpawnArea = null;
+    float sweatSpawnTimer = 0.0f;
+
+	void Start () {
+        sweatSpawnArea = GetComponent<BoxCollider2D>();
+	}
+
+	void Update () {
+        sweatSpawnTimer += Time.deltaTime;
+        if (sweatSpawnTimer >= sweatSpawnTime) {
+            SpawnDrop();
+            sweatSpawnTimer = 0.0f;
+        }
+	}
+
+    void SpawnDrop() {
+        Vector2 spawnPosition;
+        spawnPosition.x = Random.Range(sweatSpawnArea.transform.position.x - sweatSpawnArea.size.x / 2.0f,
+            sweatSpawnArea.transform.position.x + sweatSpawnArea.size.x / 2.0f);
+        spawnPosition.y = Random.Range(sweatSpawnArea.transform.position.y - sweatSpawnArea.size.y / 2.0f,
+            sweatSpawnArea.transform.position.y + sweatSpawnArea.size.y / 2.0f);
+
+        Instantiate(sweatPrefab, spawnPosition, Quaternion.identity);
+    }
+}
