8f601aa791922f35808c00db0f274666c7176678
chaosed0 <chaosed0@gmail.com>
2016-12-04 12:45:01 -0800

    Expire sweat drop

---
+++ Assets/scripts/Expires.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using DG.Tweening;
+
+class Expires : MonoBehaviour
+{
+    void Start() {
+    }
+    void Update() {
+        Sequence sequence = DOTween.Sequence();
+        sequence.AppendInterval(5.0f);
+        sequence.AppendCallback(() => UnityEngine.Object.Destroy(this));
+    }
+}
